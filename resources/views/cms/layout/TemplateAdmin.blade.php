46ec8ab67ca9bfb6aa6170f1dcbe49609b3b02e0
IPKIN9 <irwandirini@gmail.com>
2022-08-04 18:27:34 +0800

    Fixed: page title

--- resources/views/cms/layout/TemplateAdmin.blade.php
+++ resources/views/cms/layout/TemplateAdmin.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 
-    <title>Flash Able - Most Trusted Admin Template</title>
+    <title>{{config('app.name')}}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
